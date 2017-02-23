cc226e8f39d721df24716ec54df9230a8ef5eba4
david-binda <david.binovec@gmail.com>
2017-02-23 14:06:36 +0000

    Adding WPCOM_Concat_Utils::realpath static method for obtaining correct realpath on both, standard installations and subbir MS installations
    The body of the method is a copy of the code which has been added in previous commits. Since we need the same code on multiple places, it's better if we abstract it this way.

--- concat-utils.php
+++ concat-utils.php
@@ -19,4 +19,19 @@ class WPCOM_Concat_Utils {
 
 		return true;
 	}
+	public static function realpath( $url ) {
+		$url_parsed = parse_url( $url );
+		if ( true === is_multisite() && false == constant( 'SUBDOMAIN_INSTALL' ) ) {
+			$blog_details = get_blog_details();
+			if ( '/' !== $blog_details->path ) {
+				//In case of subdir multisite, we need to remove the blog's path from the style's path in order to be able to find the file
+				$realpath = realpath( ABSPATH . str_replace( $blog_details->path, '', $url_parsed['path'] ) );
+			} else {
+				$realpath = realpath( ABSPATH . $url_parsed['path'] );
+			}
+		} else {
+			$realpath = realpath( ABSPATH . $url_parsed['path'] );
+		}
+		return $realpath;
+	}
 }
