7330c4fca68b073ad299a232ff01d96f7f9b9076
David Binovec <david.binovec@gmail.com>
2017-05-15 16:08:29 +0000

    Enhancing the regular expression used for replacing relative paths to absolute ones the way it really only excluded the ones starting with a hash.
    The regular expression introduced in 2e29e538c7a0fb9ad73f68814e992fc5d611eef1 is excluding not only URLs starting with a hash, but also any URLs containg a hash, which is not correct and prevents those from being properly served.
    
    The negative lookbehind used for catching a hash has been extended the way it looks for a hash preceding either with `(`, `"` or `'` in order to match only URLs starting with a hash, not URLs containing a hash.
    
    This fixes test cases introduced in previous commits.

--- concat-utils.php
+++ concat-utils.php
@@ -41,7 +41,7 @@ class WPCOM_Concat_Utils {
 	public static function relative_path_replace( $buf, $dirpath ) {
 		// url(relative/path/to/file) -> url(/absolute/and/not/relative/path/to/file)
 		$buf = preg_replace(
-			'/(:?\s*url\s*\()\s*(?:\'|")?\s*([^\/\'"\s\)](?:(?<!data:|http:|https:|#|%23).)*)[\'"\s]*\)/isU',
+			'/(:?\s*url\s*\()\s*(?:\'|")?\s*([^\/\'"\s\)](?:(?<!data:|http:|https:|[\(\'"]#|%23).)*)[\'"\s]*\)/isU',
 			'$1' . ( $dirpath == '/' ? '/' : $dirpath . '/' ) . '$2)',
 			$buf
 		);
